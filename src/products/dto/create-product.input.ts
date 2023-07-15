09c4ef36edd004150dc09fbc6ed1035d33f811f2
roderickSanchez <roderick.sanchez@justo.mx>
2023-07-15 12:31:47 -0500

    create user active by default

--- src/products/dto/create-product.input.ts
+++ src/products/dto/create-product.input.ts
@@ -31,7 +31,7 @@ export class CreateProductInput {
   @Field(() => Boolean, { nullable: true })
   @IsBoolean()
   @IsOptional()
-  isActive? = false;
+  isActive? = true;
 
   @Field(() => ID)
   @IsUUID()
